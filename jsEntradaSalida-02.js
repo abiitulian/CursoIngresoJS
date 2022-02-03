0fca230119674e601d703a0d275bd5a68d249849
abiitulian <98909009+abiitulian@users.noreply.github.com>
2022-02-03 16:21:01 -0300

    Add files via upload
    ejercicio 2

---
+++ jsEntradaSalida-02.js
@@ -0,0 +1,18 @@
+/*
+Tulian Abril
+
+Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+
+function mostrar()
+{
+	/* para declara una variable se une la palabra reservada let */
+
+
+	let nombre; 
+
+    nombre=	prompt("ingrese su nombre");
+
+	alert(nombre);
+}
+
